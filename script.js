b8933064740071b3a2c6cc6dbc8bfb86e578ed5d
xstar97 <dev.xstar97@gmail.com>
2023-12-24 12:17:07 -0500

    undo

--- script.js
+++ script.js
@@ -14,53 +14,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Append the default query date as today's date if no date is provided
   if (!urlParams.has('date')) {
-    window.history.replaceState({}, document.title, `?date=${defaultDate}`);
+      window.history.replaceState({}, document.title, `?date=${defaultDate}`);
   }
 
-  // Initialize Pikaday
-  const dateElement = document.getElementById('date');
-  const datePicker = new Pikaday({
-    field: dateElement,
-    format: 'YYYY-MM-DD',
-    onSelect: function (date) {
-      const selectedDate = date.toISOString().split('T')[0];
-      window.history.replaceState({}, document.title, `?date=${selectedDate}`);
-      fetchData(selectedDate);
-    },
-  });
-
-  fetchData(requestedDate);
-
-  function fetchData(requestedDate) {
-    const apiUrl = `https://worker.apd.xstar97thenoob.com?date=${requestedDate}`;
-    fetch(apiUrl)
+  // Uncomment the next line to use the API endpoint
+  const apiUrl = `https://worker.apd.xstar97thenoob.com?date=${requestedDate}`;
+  fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        // Hide loading view and show content view
-        loadingView.style.display = 'none';
-        const contentView = document.getElementById('content');
-        contentView.style.display = 'block';
+          // Hide loading view and show content view
+          loadingView.style.display = 'none';
+          const contentView = document.getElementById('content');
+          contentView.style.display = 'block';
 
-        updateUI(data);
+          updateUI(data);
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
-        // Handle error and hide loading view
-        loadingView.style.display = 'none';
+          console.error('Error fetching data:', error);
+          // Handle error and hide loading view
+          loadingView.style.display = 'none';
       });
-  }
-
-  function updateUI(data) {
-    document.getElementById('title').innerText = data.title;
-    document.getElementById('date').innerText = data.date;
-    document.getElementById('description').innerText = data.explanation;
-    document.getElementById('image').src = data.url;
-
-    const copyrightElement = document.getElementById('copyright');
-    if (data.hasOwnProperty('copyright') && data.copyright) {
-      copyrightElement.innerText = 'Copyright: ' + data.copyright;
-    } else {
-      copyrightElement.style.display = 'none'; // Hide copyright element
-    }
-  }
 });
+
+function updateUI(data) {
+  document.getElementById('title').innerText = data.title;
+  document.getElementById('date').innerText = data.date;
+  document.getElementById('description').innerText = data.explanation;
+  document.getElementById('image').src = data.url;
+
+  const copyrightElement = document.getElementById('copyright');
+  if (data.hasOwnProperty('copyright') && data.copyright) {
+      copyrightElement.innerText = 'Copyright: ' + data.copyright;
+  } else {
+      copyrightElement.style.display = 'none'; // Hide copyright element
+  }
+}
