aafd9795eb2f2077e856c2ca087ffba41be6511a
xstar97 <dev.xstar97@gmail.com>
2023-12-24 11:38:56 -0500

    update

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,20 @@
 export async function onRequest(event) {
-    const url = "https://worker.apd.xstar97thenoob.com";
+    const apiUrl = "https://worker.apd.xstar97thenoob.com";
+    const url = new URL(apiUrl);
+
+    // Get date from the query parameter
+    const urlParams = new URLSearchParams(event.request.url);
+    const requestedDate = urlParams.get('date');
+
+    // Append date to the URL if available
+    if (requestedDate) {
+        url.searchParams.set('date', requestedDate);
+    }
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(url.toString());
         const data = await response.text();
-        
+
         // You can modify the response as needed, for example, setting a specific content type.
         const headers = {
             "Content-Type": "text/plain",
