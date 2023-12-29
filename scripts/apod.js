99993a7b4d8933dc1cdd2577a35587cedcf16909
xstar97 <dev.xstar97@gmail.com>
2023-12-28 23:14:46 -0500

    update

---
+++ scripts/apod.js
@@ -0,0 +1,59 @@
+// Function to validate if the given string is a valid date
+function isValidDate(dateString) {
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    return regex.test(dateString);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Show loading view
+    const loadingView = document.getElementById('loading');
+    loadingView.style.display = 'block';
+
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
+    const defaultDate = yyyy + '-' + mm + '-' + dd;
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const requestedDate = urlParams.get('date') || defaultDate;
+
+    // Validate and sanitize user input for the date parameter
+    if (!isValidDate(requestedDate)) {
+        // Handle invalid date, for example, redirect to the default page
+        window.location.href = '/';
+        return;
+    }
+
+    // Uncomment the next line to use the API endpoint
+    const apiUrl = `https://api.apod.xstar97thenoob.com?date=${requestedDate}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Hide loading view and show content view
+            loadingView.style.display = 'none';
+            const contentView = document.getElementById('content');
+            contentView.style.display = 'block';
+
+            updateUI(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            // Provide user-friendly error message or log more details about the error
+            loadingView.style.display = 'none';
+        });
+});
+
+function updateUI(data) {
+    document.getElementById('title').innerText = data.title;
+    document.getElementById('date').innerText = data.date;
+    document.getElementById('description').innerText = data.explanation;
+    document.getElementById('image').src = data.url;
+
+    const copyrightElement = document.getElementById('copyright');
+    if (data.copyright) {
+        copyrightElement.innerText = 'Copyright: ' + data.copyright;
+    } else {
+        copyrightElement.style.display = 'none'; // Hide copyright element
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,85 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dateGrid = document.getElementById('dateGrid');
-    const todayView = document.getElementById('todayView');
+    const yearInput = document.getElementById('yearInput');
+    const monthInput = document.getElementById('monthInput');
+    const dayInput = document.getElementById('dayInput');
+    const submitButton = document.getElementById('submitButton');
 
     // Get the current date
     const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const currentDay = String(currentDate.getDate()).padStart(2, '0');
 
-    // Set the end date to 1995-07-01
-    const endDate = new Date('1995-07-01');
+    // Set the current date in the input fields
+    yearInput.value = currentYear;
+    monthInput.value = currentMonth;
+    dayInput.value = currentDay;
 
-    // Create a grid of dates starting from tomorrow and ending at 1995-07-01
-    while (currentDate > endDate) {
-        currentDate.setDate(currentDate.getDate() - 1); // Move to the previous day
-        dateGrid.appendChild(createDateItem(currentDate));
-    }
+    // Handle submit button click
+    submitButton.addEventListener('click', () => {
+        // Get the values from the input fields
+        const year = yearInput.value;
+        const month = monthInput.value.padStart(2, '0');
+        const day = dayInput.value.padStart(2, '0');
 
-    // Display today's date at the top
-    const todayFormatted = formatDate(new Date());
-    todayView.textContent = `Today: ${todayFormatted}`;
-    todayView.addEventListener('click', () => {
-        loadItem(todayFormatted);
-    });
-
-    // Handle date item click
-    dateGrid.addEventListener('click', (event) => {
-        const target = event.target;
-        if (target.classList.contains('date-item')) {
-            const requestedDate = target.textContent;
-            loadItem(requestedDate);
+        // Validate the entered date
+        const enteredDate = new Date(`${year}-${month}-${day}`);
+        if (isNaN(enteredDate.getTime())) {
+            alert('Please enter a valid date.');
+            return;
         }
+
+        // Open a new page with the specified date
+        const requestedDate = `${year}-${month}-${day}`;
+        queryItem(requestedDate);
     });
 });
 
-function loadItem(requestedDate) {
-    // Uncomment the next line to use the API endpoint
-    const apiUrl = `https://api.apod.xstar97thenoob.com/?date=${requestedDate}`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Load item_apod.html if the date is valid
-            const itemContainer = document.getElementById('itemContainer');
-            const itemScript = document.createElement('script');
-            itemScript.src = '/scripts/item.js';
-            itemScript.onload = function () {
-                updateUI(data);
-                // Replace the current page with item_apod.html
-                window.location.replace('/item_apod.html');
-            };
-            itemContainer.appendChild(itemScript);
-        })
-        .catch(error => {
-            // Load item_error.html in case of API failure
-            const itemContainer = document.getElementById('itemContainer');
-            const itemScript = document.createElement('script');
-            itemScript.src = 'item_error.html';
-            itemContainer.appendChild(itemScript);
-            console.error('Error fetching data:', error);
-            // Replace the current page with item_error.html
-            window.location.replace('/item_error.html');
-        });
-}
+function queryItem(requestedDate) {
+    // Construct the URL with the .html extension
+    const apodUrl = `/apod.html?date=${requestedDate}`;
 
-
-// Function to create date item
-function createDateItem(date) {
-    const formattedDate = formatDate(date);
-    const dateItem = document.createElement('div');
-    dateItem.className = 'date-item';
-    dateItem.textContent = formattedDate;
-    return dateItem;
-}
-
-// Function to format date
-function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
-
-// Function to update UI
-function updateUI(data) {
-    // Your existing updateUI function
-    // ...
+    // Navigate to the APOD page with the specified date
+    window.location.href = apodUrl;
 }
